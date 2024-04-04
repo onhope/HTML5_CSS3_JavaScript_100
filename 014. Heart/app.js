4cfcaef9a45a6bd47cd7d9a8e599f2ed72f19f0e
onhope <chch4934@gmail.com>
2024-04-04 16:43:07 +0900

    014. Heart

--- 014. Heart/app.js
+++ 014. Heart/app.js
@@ -3,10 +3,10 @@ const redHeart = document.querySelector(".red-heart");
 
 grayHeart.addEventListener("click", () => {
   redHeart.classList.add("animation");
-  grayHeart.classList.add("fill-color")
+  redHeart.classList.add("fill-color")
 })
 
 redHeart.addEventListener("click", () => {
   redHeart.classList.remove("animation");
-  grayHeart.classList.remove("fill-color")
+  redHeart.classList.remove("fill-color")
 })
