02633b84e7009a7882d5addc28bf60487433560b
onhope <chch4934@gmail.com>
2024-04-04 17:25:54 +0900

    015. Background Changer

---
+++ 015. Background Changer/app.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("btn");
+const hex =document.getElementById("hexCode");
+
+function randomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for(i=0; i <6; i++) {
+    color += letters[Math.floor(Math.random()*16)];
+  }
+  return color;
+}
+
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = randomColor();
+  hex.innerHTML = randomColor();
+});
---
+++ 015. Background Changer/readme.md
@@ -0,0 +1,6 @@
+# 015. Background Changer
+
+클릭을 하면 배경화면 색이 변경되는 효과 
+
+<img src="./015. Background Changer.gif">
+
