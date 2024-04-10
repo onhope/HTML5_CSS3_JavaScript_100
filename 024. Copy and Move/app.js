594fc17ddd071cbd160c08c30f7ce31bc9314719
onhope <chch4934@gmail.com>
2024-04-10 15:40:26 +0900

    024. Copy and Move

---
+++ 024. Copy and Move/app.js
@@ -0,0 +1,11 @@
+const copyBtn = document.querySelector(".copyBtn");
+const moverBtn = document.querySelector(".moverBtn");
+const copyText = document.querySelector("textarea[name=copyTxt");
+const moveText = document.querySelector("textarea[name=moveTxt");
+const output = document.querySelector(".output");
+
+// console.log(copyBtn);
+// console.log(moverBtn);
+// console.log(copyText);
+// console.log(moveText);
+// console.log(output);
